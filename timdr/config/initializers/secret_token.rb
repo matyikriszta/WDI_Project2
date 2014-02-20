1d2880bdb5dc00f53c0ba7781af5515679c06bfc
Imran Sulemanji <imranolas@gmail.com>
2014-02-20 17:56:27 +0000

    timdr is born

---
+++ timdr/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timdr::Application.config.secret_token = '24ff76e46edcc73cef8b44efb4d9357168304ebc0a94729f66d49215830ce8ee02d031720f6735e4c5b46342560d4d4ec527d19ed09e68ebd22163952bb4281a'
