38b60b832744be03310f305fe68da906d4442cf3
Imran Sulemanji <imranolas@gmail.com>
2014-02-22 18:44:27 +0000

    layouts and simple styling of user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where(gender: current_user.get_preference).page(params[:page])
+    @users = User.where(gender: current_user.get_preference).page(params[:page]).per(12)
   end
 
   def search
 
     if params['distance'].blank?
-      @users =  User.where(gender: current_user.get_preference).page(params[:page])
+      @users =  User.where(gender: current_user.get_preference).page(params[:page]).per(12)
     else
-      @users = current_user.nearbys(params[:distance]).where(gender: current_user.get_preference).page(params[:page])
+      @users = current_user.nearbys(params[:distance]).where(gender: current_user.get_preference).page(params[:page]).per(12)
     end    
 
     unless params['interest_list'].blank?
-      @users = @users.tagged_with(params['interest_list']).page(params[:page])
+      @users = @users.tagged_with(params['interest_list']).page(params[:page]).per(12)
     end
     # raise
     render :index
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :location, :interest_list, :gender, :preference, :dob
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :location, :interest_list, :gender, :preference, :dob, :description
   # attr_accessible :title, :body
 
   geocoded_by :location
