8d47e6b33e60fe0ef91b1932ff34fe3343d1f1f7
Kriszta <matyi.kriszta@gmail.com>
2014-02-25 16:52:32 +0000

    added go to profile page button to dash/profile, edited layout, etc.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     if user.default_photo
       image_tag(user.default_photo.image)
     else
-      image_tag('tim.jpg')
+      image_tag('green.jpg')
     end
   end
 end
