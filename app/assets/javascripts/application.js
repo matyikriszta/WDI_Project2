90cc8c513259c864b0bdea1b57aa253061088e83
Imran Sulemanji <imranolas@gmail.com>
2014-02-26 16:09:08 +0000

    added image form popup to user profile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -77,7 +77,9 @@ $(function() {
 
   $(".best_in_place").best_in_place();
 
+  $('#upload_image').replaceWith('<a href=#image_form rel="leanModal" class="button">Upload Image</a>')
   $('#send_message').replaceWith('<a href=#message_form rel="leanModal" class="button">Send Message</a>')
+
   $("a[rel=leanModal]").leanModal();
 
   $(".fancybox").fancybox();
